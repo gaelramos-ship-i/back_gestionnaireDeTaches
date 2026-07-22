21430f1319bc6e05e7630ac3cb84a4b0644f2702
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-07-22 15:53:08 +0200

    add register and login with middleware

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
+require('./config/db')
+
+const authRoutes = require('./routes/authRoutes')
+
+app.use(express.json())
+app.use('/api/v1/auth', authRoutes)
+
 app.get('/', (req, res) => {
     res.send('Bienvenue sur mon API RESTful !')
 })
